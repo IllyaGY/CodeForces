9a882c7b9725feecca3aa96af1691e63b36a027d
IllyaGY <igordstudy12@gmail.com>
2026-02-21 23:17:33 -0800

    Div.3 719: D - Same Differences

---
+++ Div.3/719/D.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+    unordered_map<long long, long long> mp;
+    vector<int> v;
+    int tests = 0;
+    cin >> tests;
+    while (tests--) {
+        int n = 0;
+        cin >> n;
+        int i = 0;
+        long long ctr = 0;
+        while (n--) {
+            int t;
+            cin >> t;
+            v.push_back(t);
+            mp[t - i]++;
+            i++;
+        }
+        for (auto &i : mp) {
+            ctr += i.second * (i.second - 1) / 2;
+        }
+        cout << ctr;
+    }
+    return 0;
+}
